2dbc49ff74ceb91a565f9bd5831953c9bec9c043
ibaranov-WS\ibaranov <ivb@outlook.com>
2019-02-26 21:29:03 -0500

    Shiny deployed version - not finished

--- shiny/Stocks/server.R
+++ shiny/Stocks/server.R
@@ -2,7 +2,9 @@ library(shiny)
 library(shinyjs)
 library(forecast)
 library(tseries)
-
+library(lubridate)
+library(zoo)
+library(lattice)
 
 # Reading list of Nasdaq - listed stock names ans clean it
 readNasdaqListings <- function() {
@@ -22,18 +24,24 @@ readNasdaqListings <- function() {
   nasdaqTraded$Symbol <- factor(nasdaqTraded$Symbol)
   nasdaqTraded$Security.Name <- factor(nasdaqTraded$Security.Name)
   
+  print(paste("nasdaqTraded: ", str(nasdaqTraded)))
+  
   return(nasdaqTraded)
 }
 
 # Read stocks from Yahoo
-readStocks <- function(stocksList, startDate, quote = "Close", isMonthly=TRUE) {
+readStocks <- function(stocksList, startDate, quote = "Close", isMonthly=TRUE, session) {
   print("Entering readStocks")
   z <- list()
   compression <- if (isMonthly == TRUE) "m" else "d"
   len <- nrow(stocksList)
   for (i in 1:len) {
     name <- toString(stocksList[i, "Symbol"][1])
-    setProgress(1.0*i/len, message = paste("Loading",name))
+    
+    shinyWidgets::updateProgressBar(session = session, id = "pb", 
+                                    value = 100.0*i/len,
+                                    title = paste("Loading",name))
+    
     tryCatch({
       z[[name]] <- get.hist.quote(
         instrument = name, 
@@ -53,11 +61,19 @@ readStocks <- function(stocksList, startDate, quote = "Close", isMonthly=TRUE) {
 }
 
 plotForecast <- function(stockZoo, stockSymbol, freq, toForecast, isMonthly=TRUE) {
-  x <- ts(stockZoo, frequency=freq)
-  dmin <- start(z[[n]])
-  dmax <- end(z[[n]])
-  
   tryCatch({
+
+    tryCatch({
+      x <- ts(stockZoo, frequency=freq)
+    }, error=function(cond) {
+      stop(": not enough data")
+    })
+
+    # print dates
+    dmin <- start(stockZoo)
+    dmax <- end(stockZoo)
+    print(paste(stockSymbol, ": dmin=", dmin, "dmax=", dmax))
+    
     models <- list(
       mod_stl = stlm(x, s.window=freq, ic='aicc', robust=TRUE, method='ets')
     )
@@ -77,42 +93,91 @@ plotForecast <- function(stockZoo, stockSymbol, freq, toForecast, isMonthly=TRUE
   },
   error=function(cond) {
     plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
-    text(x = 0.5, y = 0.5, paste("Can't forecast",stockSymbol, "Stock"), cex = 1.6, col = "red")    
+    text(x = 0.5, y = 0.5, paste("Can't forecast",stockSymbol, ":", cond$message), cex = 1.6, col = "red")    
     print (cond)
     str(cond)
   },
   warning=function(cond) {
     plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
-    text(x = 0.5, y = 0.5, paste("Can't forecast",stockSymbol, "Stock"), cex = 1.6, col = "red")    
+    text(x = 0.5, y = 0.5, paste("Can't forecast",stockSymbol, "Stock:", cond$message), cex = 1.6, col = "red")    
     print (cond)
   })
 }
 
+#startDateDefault <- as.POSIXct("2016-01-01")
+startDateDefault <- "2016-01-01"
+
+
 # Server session logic
 shinyServer(function(input, output, session) {
 
-  #set.seed(99)
-
-  isMonthly <- TRUE
-  quote <- "Close"
-  nStocks <- 3
-  freq <- 12
-  toPredict <- 12
-  startDate <- as.POSIXct("2016-01-01")
+  shinyjs::disable("dateFrom")
   
-  withProgress(message = "Loading stocks symbols", {
-    nasdaqTraded <- readNasdaqListings()
-    stocks <- nasdaqTraded[sample(1:nrow(nasdaqTraded), nStocks),]
-    z <- readStocks(stocks, startDate, quote, isMonthly)
-  })
+  # sesion data
+  isMonthly <- reactiveVal(TRUE)
+  startDate <- reactiveVal(NA)
+  quote <- reactiveVal("Close")
+  nasdaqTraded <- reactiveVal(NULL)
+  nStocks <- reactiveVal(3)
+  freq <- reactiveVal(12)
+  toPredict <- reactiveVal(12)
   
-  print(paste("Stocks loaded:", length(z), "out of", nStocks))
-  
-  output$distPlot <- renderPlot({
-    par(mfrow = c(nStocks, 1))
-    for (n in 1:length(z)) {
-      plotForecast(z[[n]], names(z)[n], freq, toPredict, isMonthly)
+  # init ui
+  init <- function(back, compression) {
+    isMonthly(compression=="m")
+    if (isMonthly()) {
+      startDate(floor_date(Sys.Date() - years(-back), "years"))
+      updateSliderInput(session, "back", min = -10, max = -2, value = back, label = "Years Back")  
+    } else {
+      startDate(floor_date(Sys.Date() - months(-back), "months"))
+      updateSliderInput(session, "back", min = -12, max = -2, value = back, label = "Month Back")  
     }
+    updateDateInput(session, "dateFrom", value=startDate())
+    output$distPlot <- NULL
+  }  
+  
+  observe({
+    init(input$back,input$compression)
   })
+  
+  observe({
+    nStocks(input$stocksNum)
+    freq(input$freq)
+    toPredict(input$predict)
+    output$distPlot <- NULL
+    print("here")
+  })
+
+  # On "Run Forecast"
+  observeEvent(input$go, {
+    
+    # launch the modal
+    shinyWidgets::updateProgressBar(session = session, id = "pb", value = 0)
+    session$sendCustomMessage(type = 'launch-modal', "my-modal") 
+    
+    # load Nasdaq symbols (once per user session)
+    nt <- nasdaqTraded()
+    if (is.null(nt)) {
+      shinyWidgets::updateProgressBar(session = session, id = "pb", value = 5, title = "Loading Nasdaq Symbols")
+      nt = readNasdaqListings()
+      nasdaqTraded(nt)
+    }
+    
+    # Load stock series data
+    stocks <- nt[sample(1:nrow(nt), nStocks()),]
+    z <- readStocks(stocks, input$dateFrom, quote(), isMonthly(), session)
+    print(paste("Stocks loaded:", length(z), "out of", nStocks()))
+
+    # Forecast and plot stock series
+    output$distPlot <- renderPlot({
+      par(mfrow = c(nStocks(), 1))
+      for (n in 1:length(z)) {
+        plotForecast(z[[n]], names(z)[n], freq(), toPredict(), isMonthly())
+      }
+    })
+    
+    # close modal
+    session$sendCustomMessage(type = 'remove-modal', "my-modal")
+  })  
   
 })
--- shiny/Stocks/ui.R
+++ shiny/Stocks/ui.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(shinythemes)
 library(shinyjs)
+library(shinyWidgets)
 
 ## UI extensions
 jscode <- "
@@ -8,29 +9,55 @@ shinyjs.refocus = function(e_id) {
 document.getElementById(e_id).focus();
 }"
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
-  theme=shinytheme("journal"),
+  theme=shinytheme("cerulean"),
   shinyjs::useShinyjs(),
   extendShinyjs(text = jscode, functions = "refocus"),
   tags$style(HTML(" .shiny-input-container:not(.shiny-input-container-inline) { width: 100%; height: 100%; }")),
   tags$head(tags$style(type="text/css", ".container-fluid {  max-width: 1000px;}")),
   
+  # Fancy progress bar
+  # Source: https://stackoverflow.com/questions/44043475/adjust-size-of-shiny-progress-bar-and-center-it
+  tags$script("Shiny.addCustomMessageHandler('launch-modal', function(d) {$('#' + d).modal().focus();})"),
+  tags$script("Shiny.addCustomMessageHandler('remove-modal', function(d) {$('#' + d).modal('hide');})"),
+  tags$div(
+    id = "my-modal",
+    class="modal fade", tabindex="-1", `data-backdrop`="static", `data-keyboard`="false",
+    tags$div(
+      class="modal-dialog",
+      tags$div(
+        class = "modal-content",
+        tags$div(
+          class="modal-body",
+          shinyWidgets::progressBar(id = "pb", value = 0, title = "")
+        )
+      )
+    )
+  ),  
+  
   titlePanel("Stock Analyzer"),
   
-  # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      dateInput(inputId = "dateFrom", label = "From", value = seq(Sys.Date(), length=2, by = "-364 days")[2],
-               format = "yyyy-mm-dd", width = "100%")
+      actionButton("select", "Select Stocks", width = "100%"),
+      p(),              
+      actionButton("go", "Run Forecast", width = "100%", class="btn btn-primary"),
+      hr(),              
+      dateInput(inputId="dateFrom", label = "From", format = "yyyy-mm-dd", width = "100%", value = NA),
+      sliderInput("back", min=-100, max=-1, value=-3, label=""),
+      radioButtons("compression", "Aggregate Data:",
+                   c("Monthly" = "m","Daily" = "d")),
+      sliderInput("stocksNum", min=1, max=5, value=3, label="Stocks to Load"),
+      sliderInput("freq", min=6, max=24, value=12, label="Window"),
+      sliderInput("predict", min=6, max=24, value=12, label="To Predict")
+#      p(),              
+#      actionButton("updateRandom", "Load Random Stocks",width = "100%"),  
+#      actionButton("reset", "Reset", width = "100%", class="btn btn-warning")
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
-      column(12,
-        plotOutput("distPlot")
-      )
+      plotOutput("distPlot")
     )
   )
 ))
